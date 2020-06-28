ebaf4d51c304988ba05b7bebdce856b0c0f1b5c2
mday <mday@eitccorp.com>
2020-06-28 05:14:06 -0500

    fixed network connectivity issues

--- firmware/src/net/Network.cpp
+++ firmware/src/net/Network.cpp
@@ -63,9 +63,10 @@ bool Network::connect()
     Serial.printf("INFO:  Connecting to WiFi network: %s\n", WIFI_SSID);
     WiFi.mode(WIFI_MODE_STA);
     const ulong start = millis();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     do
     {
-        const wl_status_t status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        const wl_status_t status = WiFi.status();
         switch (status)
         {
             case WL_CONNECTED:
@@ -73,25 +74,33 @@ bool Network::connect()
                 uint8_t mac[6];
                 WiFi.macAddress(mac);
                 Ethernet.begin(mac, ip);
-                Serial.printf("INFO:  WiFi connected successfully, IP is: %s\n", ip.toString().c_str());
+                Serial.printf("INFO:  WiFi connected successfully in %ld millis, IP is: %s\n",
+                        millis() - start, ip.toString().c_str());
                 return true;
 
             case WL_NO_SSID_AVAIL:
+                Serial.printf("ERROR: WiFi connection failed: NO SSID AVAILABLE\n");
+                return false;
+
             case WL_CONNECT_FAILED:
+                Serial.printf("ERROR: WiFi connection failed: CONNECT FAILED\n");
+                return false;
+
             case WL_CONNECTION_LOST:
-            case WL_DISCONNECTED:
-                Serial.printf("ERROR: WiFi connection failed with status %d\n", status);
+                Serial.printf("ERROR: WiFi connection failed: CONNECTION LOST\n");
                 return false;
 
             case WL_NO_SHIELD:
-            case WL_SCAN_COMPLETED:
-                // not expected in `WIFI_MODE_STA`
-                Serial.printf("ERROR: WiFi connection failed, unexpected status %d\n", status);
+                Serial.printf("ERROR: WiFi connection failed, unexpected status NO SHIELD\n");
                 return false;
 
+            case WL_SCAN_COMPLETED:
+                Serial.printf("ERROR: WiFi connection failed, unexpected status SCAN COMPLETED\n");
+                return false;
+
+            case WL_DISCONNECTED: // usually the first status returned
             case WL_IDLE_STATUS:
-                const ulong now = millis();
-                const ulong waitTime = now - start;
+                const ulong waitTime = millis() - start;
                 if (waitTime > WIFI_CONNECT_TIMEOUT)
                 {
                     // already waited too long
@@ -100,7 +109,7 @@ bool Network::connect()
                     return false;
                 }
                 // delay a little to wait for connection
-                delay(WIFI_CONNECT_TIMEOUT - waitTime > 10 ? 10 : WIFI_CONNECT_TIMEOUT - waitTime);
+                delay(WIFI_CONNECT_TIMEOUT - waitTime > 100 ? 100 : WIFI_CONNECT_TIMEOUT - waitTime);
         }
     } while (true);
 }
